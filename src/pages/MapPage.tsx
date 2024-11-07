9503acae424ef2caf80d68c80ef2be855752094b
devkisungjang <jjangki2071@gmail.com>
2024-11-07 17:47:36 +0900

    키워드검색 기능 테스트

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,15 +1,68 @@
-import React from "react";
-import { Map, MapMarker } from "react-kakao-maps-sdk";
+import "../styles/MapPage.scss";
+import React, { useEffect, useState } from "react";
+import { Map, MarkerClusterer, MapMarker } from "react-kakao-maps-sdk";
+import useKakaoLoader from "./useKakaoLoader";
 
 const MapPage = () => {
+  const [info, setInfo] = useState();
+  const [markers, setMarkers] = useState([]);
+  const [map, setMap] = useState();
+
+  useEffect(() => {
+    if (!map) return;
+    const ps = new kakao.maps.services.Places();
+
+    ps.keywordSearch("이태원 맛집", (data, status, _pagination) => {
+      if (status === kakao.maps.services.Status.OK) {
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        const bounds = new kakao.maps.LatLngBounds();
+        let markers = [];
+
+        for (var i = 0; i < data.length; i++) {
+          // @ts-ignore
+          markers.push({
+            position: {
+              lat: data[i].y,
+              lng: data[i].x,
+            },
+            content: data[i].place_name,
+          });
+          // @ts-ignore
+          bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }
+        setMarkers(markers);
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setBounds(bounds);
+      }
+    });
+  }, [map]);
+
   return (
-    <Map
-      center={{ lat: 33.5563, lng: 126.79581 }}
-      style={{ width: "100%", height: "360px" }}
+    <Map // 로드뷰를 표시할 Container
+      center={{
+        lat: 37.566826,
+        lng: 126.9786567,
+      }}
+      style={{
+        width: "100%",
+        height: "350px",
+      }}
+      level={3}
+      onCreate={setMap}
     >
-      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-        <div style={{ color: "#000" }}>Hello World!</div>
-      </MapMarker>
+      {markers.map((marker) => (
+        <MapMarker
+          key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
+          position={marker.position}
+          onClick={() => setInfo(marker)}
+        >
+          {info && info.content === marker.content && (
+            <div style={{ color: "#000" }}>{marker.content}</div>
+          )}
+        </MapMarker>
+      ))}
     </Map>
   );
 };
